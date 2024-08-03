fbf3b0b71231223b604f8117fc0292c1af2ac871
TeeGee-TechBro <allin1acct.com@gmail.co>
2024-08-03 12:27:44 +0100

    cleaned up the app and did some adjustments.

--- app/index.tsx
+++ app/index.tsx
@@ -7,11 +7,11 @@ import { router } from 'expo-router';
 const Onboarding = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFF0a1' }}>
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, marginTop: '50%' }}>
         <Image style={{ width: 400, height: 200 }} source={require("@/assets/images/lambo.png")} />
         <View style={{ marginTop: 20 }}>
           <Text style={{ fontWeight: '700', fontSize: 32 }}>CarPoolin</Text>
-          <Text style={{ fontWeight: '600', fontSize: 16 }}>Drive & Save Money</Text>
+          <Text style={{ fontWeight: '600', fontSize: 16, lineHeight: 30 }}>Drive & Save Money</Text>
         </View>
         <Press onPress={() => router.push('/search')} style={{ marginTop: '24%', backgroundColor: '#6E6E6F', width: 50, height: 50, justifyContent: 'center', alignItems: 'center', borderRadius: 50 }}>
           <AntDesign name="arrowright" size={24} color="white" />
